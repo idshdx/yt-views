75fc623e05e0878ca1bc47e3244e347fbf0dc619
zer0lis <idzer0lis@gmail.com>
2019-01-14 22:30:30 +0200

    backend first commit, broken fe

--- youtube-views-fe/src/app/app.module.ts
+++ youtube-views-fe/src/app/app.module.ts
@@ -7,11 +7,7 @@ import { AppRoutingModule } from './app.routing';
 
 import { AppComponent } from './app.component';
 import { ComponentsModule } from './components/components.module';
-import { LoginModule } from './login/login.module';
-import { ProfileModule } from './profile/profile.module';
-import { SettingsModule } from './settings/settings.module';
 import { SharedModule } from './shared/shared.module';
-// import { AuthModule } from './auth/auth.module';
 import { CoreModule } from './core/core.module';
 import { LandingComponent } from './landing/landing.component';
 
